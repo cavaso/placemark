1231227a4ce8eb454a50a7bcad99d28ac1a590c9
Marcos Perusselo <marcosvvp@gmail.com>
2024-06-04 23:48:27 -0300

    trackers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,13 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* <!-- Google Tag Manager --> */}
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MKPS6FX5');</script>
+          {/* <!-- End Google Tag Manager --> */}
           <meta name="application-name" content="Placemark" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#000000" />
@@ -16,8 +23,25 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Source+Code+Pro&display=swap"
             rel="stylesheet"
           />
+          {/* <!-- Hotjar Tracking Code for Instant GIS --> */}
+          <script>
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:5012147,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          </script>
+
         </Head>
         <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKPS6FX5"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
